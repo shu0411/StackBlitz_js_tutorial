c8d08d5c8d2b8584055fc2dc886e61c7487e5556
shu0411 <97838417+shu0411@users.noreply.github.com>
2024-01-21 20:21:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackBlitz_js_tutorial
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/shu0411/StackBlitz_js_tutorial)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `
+<h1>JS Starter</h1>
+<h1>JS test</h1>
+<p>test</p>
+`;
+
+console.log('test');
